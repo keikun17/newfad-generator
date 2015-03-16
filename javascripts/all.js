b04a54cddb4551696b50fb0efde5d4c2940263b4
Buddy Magsipoc <keikun17@gmail.com>
2015-03-17 02:15:31 +0800

    Site updated to 77d1f2a

--- javascripts/all.js
+++ javascripts/all.js
@@ -37,7 +37,7 @@ s=E.pop(),O=E.length?E.join("/")+"/":"./",q.baseUrl=O),s=s.replace(Q,""),g.jsExt
 b.readyState)return N=b}),e=N;e&&(b||(b=e.getAttribute("data-requiremodule")),g=F[e.getAttribute("data-requirecontext")])}(g?g.defQueue:R).push([b,c,d])};define.amd={jQuery:!0};g.exec=function(b){return eval(b)};g(q)}})(this);
 (function() {
   require.config({
-    baseUrl: '/javascripts'
+    baseUrl: 'javascripts'
   });
 
   require(['first_words', 'second_words', 'third_words'], function() {
