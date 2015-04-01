cd0698ef585345d53bd8e33252f4e8006b8cd17e
Buddy Magsipoc <keikun17@gmail.com>
2015-04-01 13:49:13 +0800

    Site updated to 7c218ed

--- javascripts/third_words.js
+++ javascripts/third_words.js
@@ -1,6 +1,6 @@
 (function() {
   define('third_words', function() {
-    return ["Development", "Machine", "Design", "Methodology", "Test", "DEVOPS", "Prototyping", "Stories", "UX", "Estimation"];
+    return ["Development", "Machine", "Design", "Methodology", "Test", "DEVOPS", "Prototyping", "Stories", "UX", "Estimation", "Community", "Product"];
   });
 
 }).call(this);
