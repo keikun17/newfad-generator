d2ac75e3f6ba31a884c02202652a33a3b0bacea3
Buddy Magsipoc <keikun17@gmail.com>
2015-05-11 20:19:22 +0800

    Site updated to 8204fc5

--- javascripts/first_words.js
+++ javascripts/first_words.js
@@ -1,6 +1,6 @@
 (function() {
   define('first_words', function() {
-    return ["Behavior", "Test", "User", "Requirement", "Extreme", "Lean", "Spiral", "Agile", "Rational", "Feature", "Reversible", "No-Retreat-No-Surrender", "Pair", "Small", "Big", "Collective", "Sustainable", "Decentralized", "Domain", "LSD", "Yoga", "Buzzword", "Intellectual", "Unified", "Convention", "Configuration", "Gender-equality", "Sentient", "Minimum"];
+    return ["Full", "Behavior", "Test", "User", "Requirement", "Extreme", "Lean", "Spiral", "Agile", "Rational", "Feature", "Reversible", "No-Retreat-No-Surrender", "Pair", "Small", "Big", "Collective", "Sustainable", "Decentralized", "Domain", "LSD", "Yoga", "Buzzword", "Intellectual", "Unified", "Convention", "Configuration", "Gender-equality", "Sentient", "Minimum"];
   });
 
 }).call(this);
