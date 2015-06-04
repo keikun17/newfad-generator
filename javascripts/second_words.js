7bae01fb35308be37e890db0c4f0cbcde20d93ac
Buddy Magsipoc <keikun17@gmail.com>
2015-06-04 21:46:25 +0800

    Site updated to 8c86657

--- javascripts/second_words.js
+++ javascripts/second_words.js
@@ -1,6 +1,6 @@
 (function() {
   define('second_words', function() {
-    return ["Driven", "Programming", "Release", "Focused", "Learning", "Viable", "Surveilance", "Experience", "Equality", "Oriented", "Working"];
+    return ["Driven", "Programming", "Release", "Focused", "Learning", "Viable", "Surveilance", "Experience", "Equality", "Oriented", "Working", "Monolith"];
   });
 
 }).call(this);
